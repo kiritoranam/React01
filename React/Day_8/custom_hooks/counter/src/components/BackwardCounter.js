c89f722b7fef63ac7b90e91323f6ea3cdca62cbf
kiritoranam <91110901+kiritoranam@users.noreply.github.com>
2021-10-12 11:18:32 +0530

    Add files via upload

---
+++ React/Day_8/custom_hooks/counter/src/components/BackwardCounter.js
@@ -0,0 +1,12 @@
+
+
+import Card from './Card';
+import useCounter from './Hooks/use-counter';
+
+const BackwardCounter = () => {
+ const counter = useCounter(false);
+
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
