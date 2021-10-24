fd6efe0a63bb963ab3a308d0c6dd102b140a1d1e
kiritoranam <91110901+kiritoranam@users.noreply.github.com>
2021-10-24 13:28:31 +0530

    Add files via upload

---
+++ React/Day_9/Router/router-example2/src/App.js
@@ -0,0 +1,43 @@
+import React, { Suspense } from 'react';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuotes = React.lazy(() => import('./Pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./Pages/QuoteDetail'));
+const NotFound =React.lazy(() => import('./Pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./Pages/AllQuotes'));
+
+function App() {
+  return (
+    <Layout>
+      <Suspense 
+      fallback={
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      }>
+      <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes'/>
+      </Route>
+      <Route path='/quotes' exact>
+          <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+      </Route>
+      <Route path='/new-quote'>
+          <NewQuotes />
+      </Route>
+      <Route path='*'>
+        <NotFound />
+      </Route>
+    </Switch>
+    </Suspense>
+    </Layout>
+  );
+}
+
+export default App;
