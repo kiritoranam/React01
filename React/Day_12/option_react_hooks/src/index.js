8dd8cc78e9ed9b34b4a4f5d4e0565ae65166d559
kiritoranam <91110901+kiritoranam@users.noreply.github.com>
2024-01-21 17:31:45 +0530

    Delete React/Day_12 directory

--- React/Day_12/option_react_hooks/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import AuthContextprovider from './context/auth-context';
-
-ReactDOM.render(<AuthContextprovider><App /></AuthContextprovider>, document.getElementById('root'));
